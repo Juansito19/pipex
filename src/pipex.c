58259078401b54ab9d7da4f7a92645d0907ff463
c1r6s1 <jbrosio@c2r5s5.42malaga.com>
2025-09-23 22:39:21 +0200

    new pipex

---
+++ src/pipex.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrosio <jbrosio@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 18:46:19 by jbrosio           #+#    #+#             */
+/*   Updated: 2025/09/21 21:48:56 by jbrosio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+/* function of the first child process */
+void	first_child_process(char **av, char **envp, t_command *vars)
+{
+	close(vars->fd[0]);
+	vars->fd_read = open(av[1], O_RDONLY);
+	if (vars->fd_read < 0)
+		ft_puterr(NULL, errno, vars);
+	if (dup2(vars->fd_read, STDIN_FILENO) < 0)
+		ft_puterr(NULL, errno, vars);
+	close(vars->fd_read);
+	if (dup2(vars->fd[1], STDOUT_FILENO) < 0)
+		ft_puterr(NULL, errno, vars);
+	close(vars->fd[1]);
+	if (execve(vars->bin_in, vars->command_in, envp) < 0)
+		ft_puterr(NULL, errno, vars);
+	exit (0);
+}
+
+/* function of the second child process */
+void	second_child_process(char **av, char **envp, t_command *vars)
+{
+	close(vars->fd[1]);
+	vars->fd_write = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (vars->fd_write < 0)
+		ft_puterr(NULL, errno, vars);
+	if (dup2(vars->fd[0], STDIN_FILENO) < 0)
+		ft_puterr(NULL, errno, vars);
+	close(vars->fd[0]);
+	if (dup2(vars->fd_write, STDOUT_FILENO) < 0)
+		ft_puterr(NULL, errno, vars);
+	close(vars->fd_write);
+	if (execve(vars->bin_out, vars->command_out, envp) < 0)
+		ft_puterr(NULL, errno, vars);
+	exit (0);
+}
+
+/* function of the parent process */
+void	father_process(char **av, char **envp, t_command *vars)
+{
+	ft_find(vars, av, envp);
+	vars->pid = fork();
+	if (vars->pid < 0)
+		ft_puterr(NULL, errno, vars);
+	else if (vars->pid == 0)
+		first_child_process(av, envp, vars);
+	else
+	{
+		close(vars->fd[1]);
+		waitpid(vars->pid, NULL, 0);
+		vars->pid2 = fork();
+		if (vars->pid2 < 0)
+			ft_puterr(NULL, errno, vars);
+		else if (vars->pid2 == 0)
+			second_child_process(av, envp, vars);
+		else
+		{
+			close(vars->fd[0]);
+			waitpid(vars->pid2, NULL, 0);
+			if (vars->pid2 == 2)
+				exit (2);
+			else
+				free_all(vars);
+		}
+	}
+}
+
+/* function initialize of the struct */
+void	init_struct(t_command *vars)
+{
+	vars->fd[0] = 0;
+	vars->fd[1] = 0;
+	vars->fd_write = 0;
+	vars->fd_read = 0;
+	vars->pid = 0;
+	vars->pid2 = 0;
+	vars->path = NULL;
+	vars->bin_in = NULL;
+	vars->bin_out = NULL;
+	vars->command_in = NULL;
+	vars->command_out = NULL;
+	vars->path_dir = NULL;
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_command	vars;
+
+	errors_control(ac, av, envp);
+	init_struct(&vars);
+	if (pipe(vars.fd) == -1)
+	{
+		ft_putstr_fd(strerror(24), 2);
+		exit (24);
+	}
+	else
+		father_process(av, envp, &vars);
+	return (0);
+}
---
+++ src/pipex_errors.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_errors.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbrosio <jbrosio@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/17 19:16:44 by jbrosio           #+#    #+#             */
+/*   Updated: 2025/09/21 21:46:45 by jbrosio          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+void	ft_puterr(char *str, int error_num, t_command *vars)
+{
+	ft_putstr_fd(RED, 2);
+	ft_putstr_fd("Error : ", 2);
+	if (str)
+		ft_putstr_fd(str, 2);
+	else if (error_num == 127)
+		ft_putstr_fd("command not found\n", 2);
+	else if (error_num == 126)
+		ft_putstr_fd("command found but no executable\n", 2);
+	else
+	{
+		ft_putstr_fd(strerror(error_num), 2);
+		ft_putstr_fd("\n", 2);
+	}
+	if (vars)
+		free_all(vars);
+	ft_putstr_fd(RESET, 2);
+	exit (2);
+}
+
+int	arg_error(int ac, char **av, char **envp)
+{
+	int	i;
+
+	i = 0;
+	if (ac != 5)
+		return (1);
+	else if (!*envp)
+		ft_puterr("Error : enviroment not found\n", 0, NULL);
+	else
+	{
+		while (i < ac)
+		{
+			if (av[i][0] == '\0')
+				return (22);
+			i++;
+		}
+	}
+	return (0);
+}
+
+void	errors_control(int ac, char **av, char **envp)
+{
+	if (arg_error(ac, av, envp) == 1)
+		ft_puterr("Usage: ./pipex file1 cmd1 cmd2 file2\n", 0, NULL);
+	else if (arg_error(ac, av, envp) == 22)
+		ft_puterr(NULL, 22, NULL);
+}
